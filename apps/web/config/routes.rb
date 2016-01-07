4b375aa7c98613c0286d6f4898ea38aa028522a0
Jamie Wright <jamie@brilliantfantastic.com>
2016-01-07 17:20:40 -0500

    Ensure that the bot#ready is reserved for authenticated users

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,7 +1,7 @@
 get "/bots/ready", to: "bots#ready", as: :bots_ready
 
 resources :sessions, only: [:create, :new]
-get "/signup", to: "sessions#new"
+get "/signup", to: "sessions#new", as: :signup
 get "/auth/:provider/callback", to: "sessions#create"
 
 redirect "/", to: "/signup"
--- apps/web/controllers/bots/ready.rb
+++ apps/web/controllers/bots/ready.rb
@@ -1,11 +1,20 @@
 module Web::Controllers::Bots
   class Ready
     include Web::Action
+    before :authenticate!
 
     expose :team
 
     def call(params)
       @team = TeamRepository.find current_user.team_id
+    end
+
+    private
+
+    def authenticate!
+      require_login do
+        redirect_to routes.signup_path
+      end
     end
   end
 end
